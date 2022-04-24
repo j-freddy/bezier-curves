d5e09b9ce50d4aad731ba4c604e6688d52cc096a
j-freddy <jtf.imperial@gmail.com>
2022-04-24 23:51:44 +0100

    Clean up main.js

--- js/src/main.js
+++ js/src/main.js
@@ -3,21 +3,23 @@ const e = 0.000001; // For floating point errors
 const canvas = document.getElementById("main-canvas");
 const ctx = canvas.getContext("2d");
 
-function lerpAnimation() {
+function animateLerp() {
   GUI.animateLerp(new Point(20, 180), new Point(25, 10));
 }
 
+function animateBezier() {
+  const curve = new BezierCurve([
+    new Point(20, 180),
+    new Point(25, 10),
+    new Point(140, 10),
+    new Point(145, 160)
+  ]);
+
+  GUI.animateBezierCurve(curve, drawLines=true);
+}
+
 function main() {
-  lerpAnimation();
-
-  // const curve = new BezierCurve([
-  //   new Point(20, 180),
-  //   new Point(25, 10),
-  //   new Point(140, 10),
-  //   new Point(145, 160)
-  // ]);
-
-  // GUI.animateBezierCurve(curve, drawLines=true);
+  animateBezier();
 }
 
 window.onload = () => {
